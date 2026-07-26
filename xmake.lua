8e95bc19c6a8a0ece883db9f76ab8182d7ea8db0
MAK_MART <martinmarco813@gmail.com>
2026-07-26 22:05:52 +0200

    xmake build script

--- xmake.lua
+++ xmake.lua
@@ -11,7 +11,6 @@ add_rules("plugin.compile_commands.autoupdate")
 add_requires("rmlui 6.2", "glm 1.0.3", "glfw 3.4")
 add_requires("gtest 1.17.0")
 
-set_policy("build.c++.modules", true)
 
 if is_mode("debug") then
     set_symbols("debug")
@@ -23,6 +22,7 @@ end
 
 target("glad")
   set_kind("static")
+  set_showmenu(false)
   add_files("engine/vendor/glad/src/gl.c")
   add_includedirs("engine/vendor/glad/include")
 
@@ -30,7 +30,7 @@ target("glad")
 -- add_cxxflags("-freflection", "-fexpansion-statements", "-freflection-latest")
 target("engine")
   set_kind("static")
-  set_languages("c++latest")
+  set_languages("c++26")
   add_files("engine/**.cpp")
   add_files("engine/**.cppm", { public = true })
   remove_files("engine/vendor/**", "engine/renderer/ui/**")
@@ -56,22 +56,24 @@ for _, file in ipairs(os.files("engine/tests/test_*.cpp")) do
     local name = path.basename(file)
     target(name)
         set_kind("binary")
+        set_group("tests")
+        set_default(false)
         set_languages("c++26")
         add_deps("engine")
         add_files(file)
         add_files("engine/tests/main.cpp")
         add_packages("gtest")
-        set_default(false)
         -- add_tests("default")
-        add_tests("default", {runargs = "--gtest_list_tests --gtest_color=yes"})
+        add_tests("default", {runargs = "--gtest_color=yes"})
 end
+
 target("application_test")
   set_kind("binary")
-  set_languages("c++latest")
+  set_group("apps")
+  set_languages("c++26")
   add_deps("engine")
   add_files("engine/test_app/test.cpp")
-  add_files("engine/test_app/**.cppm")
-  set_default(false)
+  -- add_files("engine/test_app/**.cppm")
 
 
 -- target("game")
